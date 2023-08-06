15b20df19db97d96ccdcc9428fd1261bec75d4e9
Umer Farooq <umerfarooq03100276796@gmail.com>
2023-08-06 20:24:41 +0500

    elmentry code

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+// import Logo from "../moralis-logo.svg";
+import Logo from "../moralis-logo.png";
+import Eth from "../eth.svg";
+import { Link, useLocation } from "react-router-dom";
 
-function Header() {
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+  // let path = window.location.pathname;
+  let pathUrl = useLocation();
+  let path = pathUrl.pathname
+  console.log(path,"hello");
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <Link to="/" className="link">
+          <div className={`${path==="/"?"headerItem headerItemActive":"headerItem"}`}>Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className={`${path==="/tokens"?"headerItem headerItemActive":"headerItem"}`}>Tokens</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
